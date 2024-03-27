969b9c9ea8fa452c3d99665c9aab6172a3b46123
houteqwe <hout3e123@gmail.com>
2024-03-27 17:32:32 +0500

    task 5.1.6

---
+++ ConsoleApp1/Unit 2/task 5.1.6.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task516
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            int[] array = GetArrayFromConsole();
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        int temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
