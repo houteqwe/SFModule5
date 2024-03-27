b681855122e9899e4efd7c7ab006543b7ac3296a
houteqwe <hout3e123@gmail.com>
2024-03-27 17:14:21 +0500

    task 515

---
+++ ConsoleApp1/Unit 2/task 5.1.5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task515
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            string[] favcolors = new string[3];
+
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
+        }
+
+        static string ShowColor()
+        {
+            Console.WriteLine("Введите цвет:");
+            string color = Console.ReadLine();
+            return color;
+        }
+    }
+}
